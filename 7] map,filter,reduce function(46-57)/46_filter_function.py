3f66ebf594f51091f93c2217254beedde5910be2
Prathamesh <prathameshgurav250706@gmail.com>
2026-04-25 19:05:27 +0530

    numpy basic with file handling

--- 7] map,filter,reduce function(46-57)/46_filter_function.py
+++ 7] map,filter,reduce function(46-57)/46_filter_function.py
@@ -1,11 +1,11 @@
 # # display the elements from the list whose letter starts with 'b' and 's'
-def show(n):
-    if n.startswith('b') or n.startswith('s'):
-        print(n)
+# def show(n):
+#     if n.startswith('b') or n.startswith('s'):
+#         print(n)
         
 
-a=filter(show,['banana','apple','strawberry'])
-print(list(a))
+# a=filter(show,['banana','apple','strawberry'])
+# print(list(a))
 
 
 # print(list(filter(lambda n:True if n.startswith('b') or n.startswith('s') else False,['banana','apple','strawberry'])))
