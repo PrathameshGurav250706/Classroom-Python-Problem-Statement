cbd73cbb41f54f2edbd65ffebc365075c0d414c4
Prathamesh <prathameshgurav250706@gmail.com>
2026-04-27 13:16:14 +0530

    using for loop

--- 13] File handling (142-151 )/148_write_1to20_read_odd.py
+++ 13] File handling (142-151 )/148_write_1to20_read_odd.py
@@ -1,7 +1,7 @@
 #WAP to write 1 to 20 numbers to file and read only odd numbers
 f1=open("C:\\Users\\Prathamesh\\OneDrive\\Desktop\\Language\\Python\\148.txt","w")
-new=input("Enter 1 to 20 :")
-f1.write(new)
+for i in range(1,21):
+    f1.write(str(i) + " ")
 f1.close()
 
 f2=open("C:\\Users\\Prathamesh\\OneDrive\\Desktop\\Language\\Python\\148.txt","r")
