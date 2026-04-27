c51ef2880261c8d9159f361bfeccd555c78677e8
Prathamesh <prathameshgurav250706@gmail.com>
2026-04-27 12:30:31 +0530

    word count code update

--- 13] File handling (142-151 )/142_count_words.py
+++ 13] File handling (142-151 )/142_count_words.py
@@ -2,5 +2,13 @@
 f1=open("C:\\Users\\Prathamesh\\OneDrive\\Desktop\\Language\\Python\\142.txt")
 a=f1.read()
 print(a)
-print(a.count("hello world"))
+b=a.split()
+c={}
+for i in b:
+    if i in c:
+        c[i]=c[i]+1
+    else:
+        c[i]=1
+print(c)
+
 f1.close()
